605ed11016ec924dd7ad0087a10af6b37be9c99e
theharpretsingh <110823944+theharpretsingh@users.noreply.github.com>
2023-10-01 10:08:29 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name = "myrg"
+    resource_group_name = "terraform-rg-${random_string.myrandom.id}"
     storage_account_name = "ethanssa"
     container_name = "mycontainer"
     key = "terraform.tfstate"
